1d0e9c35be7ec3f8786832360513f6c9d53ebed2
ja3noon <65400256+ja3noon@users.noreply.github.com>
2020-05-30 00:20:58 +0200

    add last changes

--- app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("keydown", function(e) {
-  var audio = document.querySelector("audio[data-key='" + e.keyCode + "']");
-  if (!audio) return;
-  document
-    .querySelector("span[data-key='" + e.keyCode + "']")
-    .classList.add("playing");
-  console.log(audio);
-  audio.play();
-});
